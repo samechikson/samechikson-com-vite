96b3b5d8ecdca8a0246fb4437ee4fb00de7dc7f7
Sam Echikson <sechikson@takescoop.com>
2024-10-18 13:23:22 -0700

    Add footer

--- src/app/Sidepanel.tsx
+++ src/app/Sidepanel.tsx
@@ -17,65 +17,73 @@ function Sidepanel({}: Props) {
         {isOpen ? "⬅️" : "➡️"}
       </button>
       <div className={["sidepanel", isOpen ? "open" : "closed"].join(" ")}>
-        <h1>Hello</h1>
-        <h3>
-          I'm a creative frontend engineer, previously at{" "}
-          <a target="_blank" href="https://scoopcommute.com">
-            Scoop
-          </a>{" "}
-          and the{" "}
-          <a target="_blank" href="https://flexindex.com">
-            Flex Index
-          </a>
-          .
-        </h3>
-
-        <div className="icons">
-          <a
-            className="tooltip"
-            target="_blank"
-            href="https://tour-pins.com"
-            title="I built Tour Pins, software to generate professional grade golf tournament pin sheets"
-          >
-            <img width={32} src={TourPinsIcon} alt="Tour Pins Icon" />
-          </a>
-          <a
-            target="_blank"
-            href="https://www.strava.com/athletes/70959606"
-            title="In my free time, I foil and ski"
-            className="tooltip"
-          >
-            <img width={32} src={StravaIcon} alt="Strava Icon" />
-          </a>
-          <a
-            target="_blank"
-            href="https://www.usga.org/content/usga/home-page/championships/2018/u-s--mid-amateur/articles/belgium_s-echikson-doesnt-come-into-first-mid-am-with-blind-fait.html"
-            title="I played professional golf"
-            className="tooltip"
-          >
-            <img width={32} src={golfIcon} alt="Golf Icon" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/samechikson/"
-            title="Linkedin"
-          >
-            <img width={32} src={linkedinIcon} alt="LinkedIn Icon" />
-          </a>
-          <a target="_blank" href="./Sam_Echikson_resume.pdf" title="Resume">
-            <img width={32} src={resumeIcon} alt="Resume Icon" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://www.behance.net/samechikson"
-            title="Visual portfolio"
-            className="tooltip"
-          >
-            <img width={32} src={behanceIcon} alt="Behance Icon" />
-          </a>
+        <div className="content">
+          <h1>Hello</h1>
+          <h3>
+            I'm a creative frontend engineer, previously at{" "}
+            <a target="_blank" href="https://scoopcommute.com">
+              Scoop
+            </a>{" "}
+            and the{" "}
+            <a target="_blank" href="https://flexindex.com">
+              Flex Index
+            </a>
+            .
+          </h3>
+          <div className="icons">
+            <a
+              className="tooltip"
+              target="_blank"
+              href="https://tour-pins.com"
+              title="I built Tour Pins, software to generate professional grade golf tournament pin sheets"
+            >
+              <img width={32} src={TourPinsIcon} alt="Tour Pins Icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.strava.com/athletes/70959606"
+              title="In my free time, I foil and ski"
+              className="tooltip"
+            >
+              <img width={32} src={StravaIcon} alt="Strava Icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.usga.org/content/usga/home-page/championships/2018/u-s--mid-amateur/articles/belgium_s-echikson-doesnt-come-into-first-mid-am-with-blind-fait.html"
+              title="I played professional golf"
+              className="tooltip"
+            >
+              <img width={32} src={golfIcon} alt="Golf Icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/samechikson/"
+              title="Linkedin"
+            >
+              <img width={32} src={linkedinIcon} alt="LinkedIn Icon" />
+            </a>
+            <a target="_blank" href="./Sam_Echikson_resume.pdf" title="Resume">
+              <img width={32} src={resumeIcon} alt="Resume Icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.behance.net/samechikson"
+              title="Visual portfolio"
+              className="tooltip"
+            >
+              <img width={32} src={behanceIcon} alt="Behance Icon" />
+            </a>
+          </div>
         </div>
+
+        <footer>
+          <p>
+            Built with{" "}
+            <a target="_blank" href="https://threejs.org" title="Three.js">
+              Three.js
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
